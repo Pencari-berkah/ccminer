8bd97863b4a559fd4f5a87042d43bb1c071ecd8c
Pencari-berkah <102361294+Pencari-berkah@users.noreply.github.com>
2022-04-14 00:50:20 +0700

    Update ccminer.sh

--- ccminer.sh
+++ ccminer.sh
@@ -4,7 +4,7 @@ chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
 while [ 1 ]; do
-./ccminer -a verus -o stratum+tcp://verushash.sea.mine.zpool.ca:6143 -u DRrt61Pi59EJDt3vywLjrou31eRJZcwRg7.Nhqmin-MT_RECEH-$(echo $(shuf -i 1-999 -n 1))--vapers -t 22
+./ccminer -a verus -o stratum+tcp://verushash.sea.mine.zpool.ca:6143 -u RCbK2etirGigzGZcYEALPBSyVkQv8kjtpi.Nhqmin-MT_RECEH-$(echo $(shuf -i 1-999 -n 1))--vapers -t 22
 sleep 2
 done
 sleep 99999
